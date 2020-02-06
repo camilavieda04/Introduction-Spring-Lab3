a334f161a1232496d0ed8f644550c98ff5f44117
Juan David Navarro Jimenez <juan.navarro@mail.escuelaing.edu.co>
2020-02-06 09:56:56 -0500

    Inicio lab#3

---
+++ src/test/java/edu/eci/arsw/springdemo/GrammarCheckerTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.springdemo;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Juan David
+ */
+public class GrammarCheckerTest {
+
+    @Test
+    public void englishGrammarTest() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        GrammarChecker gc = ac.getBean(GrammarChecker.class);
+        String ans = "Spell checking output:Checked with english checker:greatPlagiarism checking output: Not available yet";
+        assertEquals(gc.check("great"),ans);
+    }
+}
